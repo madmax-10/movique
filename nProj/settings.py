fc14c03d83dcd4377074a364e677fdf0a5a401e1
madmax-10 <ashbhatta234@gmail.com>
2025-05-21 06:05:27 -0500

    h

--- nProj/settings.py
+++ nProj/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-AALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 # Application definition
 
 INSTALLED_APPS = [
